a1ba6c95e93f9f6831fcdcb7d47e6af6395cfcc2
mikeyrayvon <mikey@interglobal.vision>
2021-04-09 12:49:08 +0200

    quantum direction

--- pages/index.js
+++ pages/index.js
@@ -7,32 +7,54 @@ import Container from '../components/Container'
 
 const Home = () => {
   const [result, setResult] = useState('')
+  const [loading, setLoading] = useState(false)
   const getRandomData = (query) => {
-    console.log(query)
+    setLoading(true)
     postData('/api/qrng', query)
     .then(res => {
       console.log(res)
-      setResult(res.data[0])
+      const dir = getDirection(res.data[0])
+      setResult(dir)
+      setLoading(false)
     })
     .catch(error => {
       console.error(error)
+      setResult('Error')
+      setLoading(false)
     })
   }
 
+  const isInSeq = (s, t) => Number.isInteger((t - s) / 4)
+
+  const getDirection = (t) => {
+    console.log(t)
+    if (isInSeq(1, t)) {
+      return 'Forward'
+    } else if (isInSeq(2, t)) {
+      return 'Right'
+    } else if (isInSeq(3, t)) {
+      return 'Backward'
+    } else if (isInSeq(4, t)) {
+      return 'Left'
+    }
+  }
+
   return (
-    <div className='absolute inset-0 flex flex-col justify-center'>
+    <div className='absolute inset-0 flex flex-col justify-center bg-black'>
       <section className='py-8 text-center'>
         <Container>
-          <div className='mb-4'>
+          <div className='h-8 mb-48 bg-blue-400 p-20 text-white'><span>{loading ? (
+            'Generating...'
+          ) : result}</span></div>
+          <div>
             <button
-              className='border-2 p-2 rounded-md'
+              className='border-2 p-2 rounded-md text-white'
+              disabled={loading}
               onClick={() => getRandomData({
                 length: 1,
-                type: 'uint16',
-                size: 6
-              })}>Get Random Data</button>
+                type: 'uint8'
+              })}>Quantum Random Direction</button>
           </div>
-          <div>{result}</div>
         </Container>
       </section>
     </div>
